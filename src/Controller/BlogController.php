81cedced5650d923e0636faf7d07ccc6f820e338
David-Ljz <dvd.ljz@gmail.com>
2021-10-12 15:04:00 +0200

    Ajout pagination liste des articles

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -54,12 +56,39 @@ class BlogController extends AbstractController
     /**
      * @Route("/publications/liste/", name="publication_list", methods={"GET"})
      */
-    public function publicationList(ArticleRepository $articleRepository): Response
+    public function publicationList(Request $request, ArticleRepository $articleRepository, PaginatorInterface $paginator): Response
     {
+
+        // Récupération du numéro de la page demandée dans l'URL
+        $requestedPage = $request->query->getInt('page', 1);
+
+        // Vérification que le numéro est positif
+        if($requestedPage < 1){
+            throw new NotFoundHttpException();
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery('SELECT a FROM App\Entity\Article a ORDER BY a.publicationDate DESC');
+
+        // Récupération des articles
+        $articles = $paginator->paginate(
+            $query,
+            $requestedPage,
+            10
+        );
+
+
         return $this->render('blog/publicationList.html.twig', [
-            'articles' => $articleRepository->findAll(),
+            'articles' => $articles,
         ]);
     }
+
+
+
+
+
+
 
     /**
      * @Route("/publication/{slug}", name="publication_view")
