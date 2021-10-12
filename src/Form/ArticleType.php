018391ca3b075556b71874e63d6e9c5251781d56
David-Ljz <dvd.ljz@gmail.com>
2021-10-12 10:35:46 +0200

    Ajout et config de CKEditor

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,17 +3,57 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
+
+            // Champ titre
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci de renseigner un titre',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Le titre doit contenir au moins {{ limit }} caractères',
+                        'max' => 150,
+                        'maxMessage' => 'Le titre doit contenir au maximum {{ limit }} caractères',
+                    ]),
+                ],
+            ])
+
+            // Champ contenu
+            ->add('content', CKEditorType::class, [
+                'label' => 'Contenu',
+                'attr' => [
+                    'class' => 'd-none',
+                ],
+                'purify_html' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci de renseigner un contenu',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Le contenu doit contenir au moins {{ limit }} caractères',
+                        'max' => 50000,
+                        'maxMessage' => 'Le contenu doit contenir au maximum {{ limit }} caractères',
+                    ]),
+                ],
+            ])
+
         ;
     }
 
@@ -21,6 +61,11 @@ class ArticleType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Article::class,
+
+            // TODO: À enlever
+            'attr' => [
+                'novalidate' => 'novalidate',
+            ],
         ]);
     }
 }
